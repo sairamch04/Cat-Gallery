85b0653b437df920e7d36796df79c0bc604bc443
Sairam <sairamch04@gmail.com>
2016-07-28 19:04:01 +0530

    did cleanup

--- public/controller.js
+++ public/controller.js
@@ -1,47 +1,19 @@
 var pagecount=1;
+var isFetching=false;
 
 galleryApp.controller('ctrl',  function ($scope, authService){
 
     authService.getImages(pagecount)
-    .then(function(output) {        
-       var output=JSON.parse(output);
+    .then(function(output) {
         $scope.obj=output;
     });
-
-
-	//Decrease the page count
-    $scope.decrease =function(){
-        pagecount--;
-        if (pagecount<=0) {
-            pagecount=1;
-        }
-        authService.getImages(pagecount)
-        .then(function(output) {        
-           var output=JSON.parse(output);
-            $scope.obj=output;
-        });
-
-    };
-
-	//Increase the page count
-    $scope.increase = function(){
-        pagecount++;
-        authService.getImages(pagecount)
-        .then(function(output) {        
-           var output=JSON.parse(output);
-            $scope.obj=output;
-        });
-
-    };
 
     //scrolling 
     $scope.getMoreImages = function() {
         pagecount++;
-        console.log("called .");
         authService.getImages(pagecount)
-        .then(function(output) {        
-           var output=JSON.parse(output);
+        .then(function(output) {    
             $scope.obj=output;
         });
-      }
+  }
 }); 
